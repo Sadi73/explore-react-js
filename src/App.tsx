fcd937a774e6cd4670907a16580e5c1827d047d1
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2026-04-30 21:25:29 +0600

    this is an empty react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <p className='text-2xl font-bold underline'>Learn - Learn - Learn</p>
+      <p className='text-xl text-red-500'>This is an empty react project</p>
+    </div>
+  )
+}
+
+export default App
